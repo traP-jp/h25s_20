7254e321232434a0ba0585b9acd01fd2c2fc6c69
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-06-21 10:28:38 +0900

    feat: RoomSelectViewページのプロトタイプを作成

--- frontend/src/views/RoomSelectView.vue
+++ frontend/src/views/RoomSelectView.vue
@@ -1,3 +1,48 @@
 <template>
-  <div>RoomSelectView</div>
+  <div :class="$style.container">
+    <div :class="$style.header">Select a Room</div>
+    <div :class="$style.rooms">
+      <RoomButton
+        v-for="room in roomData"
+        :key="room.id"
+        :room="room"
+        @click="handleRoomClick(room)"
+      />
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { roomData } from "@/lib/sample-data";
+import RoomButton from "@/components/RoomButton.vue";
+
+function handleRoomClick(room) {
+  console.log("Room clicked:", room);
+}
+</script>
+
+<style module>
+.container {
+  width: 500px;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.header {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.rooms {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
