8036e10fc5569c5c567ced6401e8f8ce58bca594
kitsne241 <bicycle.y.t@gmail.com>
2025-06-21 17:27:04 +0900

    add: ボタンの設置

--- frontend/src/components/playgame/MathInput.vue
+++ frontend/src/components/playgame/MathInput.vue
@@ -1,3 +1,69 @@
 <template>
-  <input type="text" name="text" placeholder="数式を入力" />
+  <div :class="$style.container">
+    <div :class="$style.tens">
+      <div :class="$style.tensRow">
+        <MathInputButton text="1" />
+        <MathInputButton text="2" />
+        <MathInputButton text="3" />
+      </div>
+      <div :class="$style.tensRow">
+        <MathInputButton text="4" />
+        <MathInputButton text="5" />
+        <MathInputButton text="6" />
+      </div>
+      <div :class="$style.tensRow">
+        <MathInputButton text="7" />
+        <MathInputButton text="8" />
+        <MathInputButton text="9" />
+      </div>
+    </div>
+    <div :class="$style.tens">
+      <div :class="$style.symbolRow">
+        <MathInputButton icon="ic:outline-plus" />
+        <MathInputButton icon="ic:outline-minus" />
+      </div>
+      <div :class="$style.symbolRow">
+        <MathInputButton icon="ic:sharp-close" />
+        <MathInputButton icon="ic:sharp-divide" />
+      </div>
+      <div :class="$style.symbolRow">
+        <MathInputButton text="( )" />
+        <MathInputButton icon="ic:outline-backspace" />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import MathInputButton from "./MathInputButton.vue";
+</script>
+
+<style module>
+.container {
+  margin: 30px auto 0 auto;
+  width: 350px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.tens {
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.tensRow {
+  width: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.symbolRow {
+  width: 130px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
---
+++ frontend/src/components/playgame/MathInputButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="$style.container">
+    <Icon v-if="icon" :icon="icon" :class="$style.icon" />
+    <span v-else :class="$style.text">{{ text }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+defineProps<{
+  text?: string;
+  icon?: string;
+}>();
+</script>
+
+<style module>
+.container {
+  width: 60px;
+  height: 60px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #446688;
+}
+
+.text {
+  color: white;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.icon {
+  color: white;
+  font-size: 32px;
+}
+</style>
