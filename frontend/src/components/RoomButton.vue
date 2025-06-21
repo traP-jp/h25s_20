b730a4e057b0bf8170c743e713a2637779c4a69b
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-06-21 10:28:00 +0900

    feat: RoomButtonコンポーネント

---
+++ frontend/src/components/RoomButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div :class="$style.container">
+    <div>
+      <div>{{ room.name }}</div>
+      <div :class="$style.details">
+        <div>ID: {{ room.id }}</div>
+        <div>Status: {{ room.status }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  room: {
+    id: number;
+    name: string;
+    status: string;
+  };
+}>();
+</script>
+
+<style module>
+.container {
+  width: 100%;
+  border: 1px solid #ccc;
+  text-align: left;
+}
+.details {
+  padding: 10px;
+  border-top: 1px solid #ddd;
+}
+</style>
