287b45d63712c208998c54446a3d5a4bcfe62438
aki2416 <akinari.katono@gmail.com>
2025-06-21 15:41:44 +0900

    add: NewBoard,PopulateRow,PopulateColumnを追加

--- backend/internal/domain/room.go
+++ backend/internal/domain/room.go
@@ -1,5 +1,7 @@
 package domain
 
+import "math/rand"
+
 type Room struct {
 	ID         int
 	Name       string
@@ -25,4 +27,36 @@ type PlayerScore struct {
 	ID       int
 	PlayerId string
 	Score    int
+}
+
+// 新規盤面の作成
+func NewBoard() GameBoard {
+	size := 4
+	gb := &GameBoard{
+		Version: 1,
+		Board:   make([][]int, size),
+	}
+	//盤面の初期化
+	for i := range gb.Board {
+		gb.Board[i] = make([]int, size)
+	}
+	//盤面全体を1から9のランダムな整数で埋める
+	for i := range gb.Size {
+		gb.PopulateRow(i)
+	}
+	return *gb
+}
+
+// 指定の列を1から9のランダムな整数で埋める
+func (gb GameBoard) PopulateRow(row int) {
+	for i := 0; i < gb.Size; i++ {
+		gb.Board[row][i] = rand.Intn(9) + 1 //1-9の乱数
+	}
+}
+
+// 指定の行を1から9のランダムな整数で埋める
+func (gb GameBoard) PopulateColumn(col int) {
+	for i := 0; i < gb.Size; i++ {
+		gb.Board[i][col] = rand.Intn(9) + 1 // 1-9の乱数
+	}
 }
