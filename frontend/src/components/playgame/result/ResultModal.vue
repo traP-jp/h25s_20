646a187eb51403364572d26bf489a0dcccd487af
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-06-21 23:31:47 +0900

    fix: typo 関数showResultModal

--- frontend/src/components/playgame/result/ResultModal.vue
+++ frontend/src/components/playgame/result/ResultModal.vue
@@ -4,13 +4,11 @@
     <!-- ここに Component を差し込む -->
     <div>result modal</div>
   </OverlayModal>
-  <button @click="showResultModal= true">resultモーダルのテスト</button>
+  <button @click="showResultModal = true">resultモーダルのテスト</button>
 </template>
 
 <script setup lang="ts">
-
 import OverlayModal from "@/components/OverlayModal.vue";
 import { ref } from "vue";
-const showresultModal = ref(false);
-
+const showResultModal = ref(false);
 </script>
