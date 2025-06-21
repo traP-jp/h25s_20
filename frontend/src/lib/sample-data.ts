9aaba10ab31e722d66c3dbb4cb314961e8f3327c
mikannkann <82451989+mikannkann@users.noreply.github.com>
2025-06-21 10:27:23 +0900

    chore: roomDataのサンプルデータ作成

---
+++ frontend/src/lib/sample-data.ts
@@ -0,0 +1,34 @@
+export const roomData = [
+    {
+        id: 0,
+        name: 'A',
+        status: 'open',
+        // resultLog
+        // players
+    },
+    {
+        id: 1,
+        name: 'B',
+        status: 'open',
+    },
+    {
+        id: 2,
+        name: 'C',
+        status: 'open',
+    },
+    {
+        id: 3,
+        name: 'D',
+        status: 'open',
+    },
+    {
+        id: 4,
+        name: 'E',
+        status: 'open',
+    },
+    {
+        id: 5,
+        name: 'F',
+        status: 'open',
+    },
+]
