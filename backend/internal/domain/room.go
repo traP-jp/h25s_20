1857a8e99db89fa11f3cd560c1a9e4fbeae266fc
aki2416 <akinari.katono@gmail.com>
2025-06-21 15:49:24 +0900

    add UpdateLine,UpdateLinesを追加

--- backend/internal/domain/room.go
+++ backend/internal/domain/room.go
@@ -1,6 +1,9 @@
 package domain
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type Room struct {
 	ID         int
@@ -27,6 +30,12 @@ type PlayerScore struct {
 	ID       int
 	PlayerId string
 	Score    int
+}
+
+//回答確認後一致した行と列の情報を保持
+type Match struct {
+	LineType string //"row"または"col"
+	Index    int	
 }
 
 // 新規盤面の作成
@@ -59,4 +68,32 @@ func (gb GameBoard) PopulateColumn(col int) {
 	for i := 0; i < gb.Size; i++ {
 		gb.Board[i][col] = rand.Intn(9) + 1 // 1-9の乱数
 	}
+}
+
+// UpdateLine は指定された行または列を新しいランダムな数字で更新します。
+func (gb *GameBoard) UpdateLine(lineType string, index int) error {
+	if !(0 <= index && index < gb.Size) {
+		return fmt.Errorf("インデックスは0から%dの間で指定してください", gb.Size-1)
+	}
+	switch lineType {
+	case "row":
+		gb.PopulateRow(index)
+		return nil
+	case "col":
+		gb.PopulateColumn(index)
+		return nil
+	default:
+		return fmt.Errorf("lineTypeは'row'または'col'である必要があります")
+	}
+}
+
+//Matches内に保存されている行、列を更新する
+func (gb *GameBoard) UpdateLines(matches []Match) error {
+	for _, match := range matches {
+		err := gb.UpdateLine(match.LineType, match.Index)
+		if err != nil {
+			return fmt.Errorf("更新中にエラーが発生しました (%s %d): %w", match.LineType, match.Index, err)
+		}
+	}
+	return nil
 }
