38f9fb015d68aea28e76deb007fec036f893da2f
aki2416 <akinari.katono@gmail.com>
2025-06-21 14:28:33 +0900

    add: roomのドメインを追加

--- backend/internal/domain/room.go
+++ backend/internal/domain/room.go
@@ -1,1 +1,27 @@
 package domain
+
+type Room struct {
+	Id	int
+	Name	string
+	Boards	[]Board
+	IsOpened	bool
+	Players	[]Player
+	ResultLog []Result 
+}
+
+type Board struct{
+	Version	int
+	Board	[]int
+}
+
+type Result struct{
+	Id	int
+	Time	string
+	Scores	[]Playerscore
+}
+
+type Playerscore struct{
+	Id int
+	PlayerId string
+	Socre int
+}
