640fd90c7b63d4ec7eebbc89f28c4d4446e3c02d
kitsne241 <bicycle.y.t@gmail.com>
2025-06-21 22:53:50 +0900

    style: トップページのデザイン変更

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,44 +1,49 @@
 <template>
-  <div class="container">
-
-    <div :class="$style.logo">
-      <img src="/logo.svg" alt="Logo">
-    </div>
-
-    <h2>名前を入れてはじめよう！</h2>
-    <div :class="$style.user_info">
+  <div :class="$style.container">
+    <img :class="$style.logo" src="/logo.svg" alt="Logo" />
+    <div :class="$style.userInfo">
       <UserIcon :id="username" :size="50" />
-      <input v-model="username" required type="text" placeholder="名前を入力" />
+      <input :class="$style.input" v-model="username" required type="text" placeholder="名前を入力" />
     </div>
-    <button :class="$style.button" @click="onClick">ゲームをはじめる</button>
-
+    <button :disabled="!isValid" :class="$style.button" @click="onClick">ゲームをはじめる</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
 import UserIcon from "@/components/UserIcon.vue";
 
-const username = ref('');
+const username = ref("");
 const router = useRouter();
 
 const onClick = () => {
   router.push("/");
-}
+};
+
+const isValid = computed(() => {
+  return username.value.trim().length >= 1 && username.value.trim().length <= 32;
+});
 </script>
 
 <style module>
-
 .container {
-  height: 100vh;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-.logo img {
+.guide {
+  text-align: center;
+}
+
+.logo {
   width: 300px;
 }
 
-.user_info {
+.userInfo {
+  margin: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -46,10 +51,13 @@ const onClick = () => {
   gap: 20px;
 }
 
+.input {
+  border-bottom: 1px solid #bbb;
+}
+
 .icon img {
   width: 60px;
   height: 60px;
   clip-path: circle(50%);
 }
-
 </style>
