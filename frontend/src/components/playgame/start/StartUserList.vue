787f2cae3d388cc01ba71c7d0258a8444ac849ff
rurun <docile.notepad.0n@icloud.com>
2025-06-22 10:59:31 +0900

    feat: crwate readyIcon

--- frontend/src/components/playgame/start/StartUserList.vue
+++ frontend/src/components/playgame/start/StartUserList.vue
@@ -8,17 +8,20 @@
       :gold="player.gold"
       :silver="player.silver"
       :bronze="player.bronze"
+      :isReady="player.isReady"
     />
   </div>
+
+
 </template>
 <script setup lang="ts">
 import StartUser from "@/components/playgame/start/StartUser.vue";
 import { ref } from "vue";
 
 const players = [
-  { id: 0, name: "kitsne", gold: 3, silver: 2, bronze: 1 },
-  { id: 1, name: "mikannkann", gold: 2, silver: 1, bronze: 0 },
-  { id: 2, name: "rurun", gold: 1, silver: 0, bronze: 0 },
+  { id: 0, name: "kitsne", gold: 3, silver: 2, bronze: 1, isReady: true },
+  { id: 1, name: "mikannkann", gold: 2, silver: 1, bronze: 0, isReady: false },
+  { id: 2, name: "rurun", gold: 1, silver: 0, bronze: 0, isReady: true },
 ];
 </script>
 
